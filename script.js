209242a76e354fe4a82259756251c4ddc010c9b5
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2022-12-26 16:41:10 +0530

    code cleanup

--- script.js
+++ script.js
@@ -1,177 +1,141 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibGFrc2h5YWplZXQiLCJhIjoiY2tuMWM2amttMHN0NDJ3cXVxOGJsY3p4MiJ9.LuGi_8FfhyDQHtWqHRgcjw';
+mapboxgl.accessToken =
+	"pk.eyJ1IjoibGFrc2h5YWplZXQiLCJhIjoiY2tuMWM2amttMHN0NDJ3cXVxOGJsY3p4MiJ9.LuGi_8FfhyDQHtWqHRgcjw";
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/lakshyajeet/clc3bmrhi005y14s1vk7tqr45',
-    center: [79.51, 29.1869],
-    zoom: 12,
-    doubleClickZoom: false,
-    hash: true,
-    maxPitch: 45,
+	container: "map",
+	style: "mapbox://styles/lakshyajeet/clc3bmrhi005y14s1vk7tqr45",
+	center: [79.51, 29.1869],
+	zoom: 12,
+	doubleClickZoom: false,
+	hash: true,
+	maxPitch: 45,
 });
 
 map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true,
-        showUserHeading: true
-    }), 'bottom-right'
+	new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true,
+		},
+		trackUserLocation: true,
+		showUserHeading: true,
+	}),
+	"bottom-right"
 );
 
-map.on('load', () => {
-    map.addSource('route', {
-        'type': 'geojson',
-        'data': "data/H1.geojson"
-    });
-
-    map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'route',
-        'layout': {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        'paint': {
-            'line-color': '#05cb63',
-            'line-width': 8,
-            'line-opacity': 0.8,
-        }
-    }, "road-label-navigation");
-
-    map.loadImage(
-        'media/logo.png',
-        (error, image) => {
-            if (error) throw error;
-
-            // Add the image to the map style.
-            map.addImage('cat', image);
-
-            // Add a data source containing one point feature.
-            map.addSource('point', {
-                'type': 'geojson',
-                'data': {
-                    'type': 'FeatureCollection',
-                    'features': [
-                        {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [79.51564595103264,
-                                    29.12450375392585]
-                            }
-                        }
-                    ]
-                }
-            });
-
-            // Add a layer to use the image to represent the data.
-            map.addLayer({
-                'id': 'points',
-                'type': 'symbol',
-                'source': 'point', // reference the data source
-                'layout': {
-                    'icon-image': 'cat', // reference the image
-                    'icon-size': [
-                        "interpolate",
-                        ["linear"],
-                        ["zoom"],
-                        0, .01,
-                        12,
-                        0.05,
-                        22,
-                        0.20
-                    ]
-                }
-            });
-        }
-    );
-});
-
-var busNo = "H";
-var shiftNo = "1";
-const busList = document.getElementById('bus');
-const busInputs = busList.getElementsByTagName('input');
+const busInputs = document.getElementById("bus").getElementsByTagName("input");
+const shiftInputs = document
+	.getElementById("shift")
+	.getElementsByTagName("input");
 
 for (const input of busInputs) {
-    input.onclick = (bus) => {
-        busNo = bus.target.value;
-        map.removeLayer('route')
-        map.removeSource('route')
-
-        map.addSource('route', {
-            'type': 'geojson',
-            'data': `data/${busNo + shiftNo}.geojson`
-        });
-        map.addLayer({
-            'id': 'route',
-            'type': 'line',
-            'source': 'route',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#05cb63',
-                'line-width': 8,
-                'line-opacity': 0.8,
-            }
-        }, "road-label-navigation");
-
-    };
+	input.onclick = (bus) => {
+		busNo = bus.target.value;
+		changeBusRoute();
+	};
+	if (input.checked) {
+		busNo = input.value;
+	}
 }
-
-const shiftList = document.getElementById('shift');
-const shiftInputs = shiftList.getElementsByTagName('input');
 
 for (const input of shiftInputs) {
-    input.onclick = (shift) => {
-        shiftNo = shift.target.value;
-        map.removeLayer('route')
-        map.removeSource('route')
-
-        map.addSource('route', {
-            'type': 'geojson',
-            'data': `data/${busNo + shiftNo}.geojson`
-        });
-        map.addLayer({
-            'id': 'route',
-            'type': 'line',
-            'source': 'route',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#05cb63',
-                'line-width': 8,
-                'line-opacity': 0.8,
-            }
-        }, "road-label-navigation");
-
-    };
+	input.onclick = (shift) => {
+		shiftNo = shift.target.value;
+		changeBusRoute();
+	};
+	if (input.checked) {
+		shiftNo = input.value;
+	}
 }
 
+function changeBusRoute() {
+	if (map.getLayer("route")) {
+		map.removeLayer("route");
+	}
+	if (map.getSource("route")) {
+		map.removeSource("route");
+	}
+	map.addSource("route", {
+		type: "geojson",
+		data: `data/${busNo + shiftNo}.geojson`,
+	});
+	map.addLayer(
+		{
+			id: "route",
+			type: "line",
+			source: "route",
+			layout: {
+				"line-join": "round",
+				"line-cap": "round",
+			},
+			paint: {
+				"line-color": "#05cb63",
+				"line-width": 8,
+				"line-opacity": 0.8,
+			},
+		},
+		"road-label-navigation"
+	);
+}
+
+map.on("load", () => {
+	changeBusRoute();
+	//GEHU Icon
+	map.loadImage("media/logo.png", (error, image) => {
+		if (error) throw error;
+		map.addImage("cat", image);
+		map.addSource("point", {
+			type: "geojson",
+			data: {
+				type: "FeatureCollection",
+				features: [
+					{
+						type: "Feature",
+						geometry: {
+							type: "Point",
+							coordinates: [79.51564595103264, 29.12450375392585],
+						},
+					},
+				],
+			},
+		});
+		map.addLayer({
+			id: "points",
+			type: "symbol",
+			source: "point",
+			layout: {
+				"icon-image": "cat",
+				"icon-size": [
+					"interpolate",
+					["linear"],
+					["zoom"],
+					0,
+					0.01,
+					12,
+					0.05,
+					22,
+					0.2,
+				],
+			},
+		});
+	});
+});
+
 var lat, lng;
-const marker = new mapboxgl.Marker()
-    .setLngLat([0, 0])
-    .addTo(map);
+const marker = new mapboxgl.Marker().setLngLat([0, 0]).addTo(map);
 
 pubnub = new PubNub({
-    subscribeKey: "sub-c-10e0e350-30c8-4f8c-84dc-659f6954424e",
-    uuid: "client"
+	subscribeKey: "sub-c-10e0e350-30c8-4f8c-84dc-659f6954424e",
+	uuid: "webClient",
 });
 pubnub.subscribe({
-    channels: ["h_bus"]
-})
+	channels: ["h_bus"],
+});
 pubnub.addListener({
-    message: function (message) {
-        console.log(message)
-        lat = message.message.lat;
-        lng = message.message.lng;
-        map.setCenter([lng, lat]);
-        marker.setLngLat([lng, lat]);
-    }
-})
+	message: function (message) {
+		console.log(message);
+		lat = message.message.lat;
+		lng = message.message.lng;
+		map.flyTo([lng, lat]);
+		marker.setLngLat([lng, lat]);
+	},
+});
