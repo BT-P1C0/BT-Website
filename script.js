0724813c92df31f1c671537448d728e27dda8629
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2022-12-27 20:31:54 +0530

    vibration test

--- script.js
+++ script.js
@@ -189,6 +189,7 @@ var parsedUtcTime = { hours: 0, minutes: 0, seconds: 0 };
 // Bus marker
 const busMarkerPopup = new mapboxgl.Popup();
 const busMarkerElement = document.createElement("div");
+busMarkerElement.onclick = navigator.vibrate(200);
 busMarkerElement.className = "bus-marker-element";
 const busMarker = new mapboxgl.Marker({
 	element: busMarkerElement,
